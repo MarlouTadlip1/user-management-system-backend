1ffe923542ff7bc01193c08bde9bd3b5dd135a5e
JonaidAsal <jonaidasal3@gmail.com>
2025-05-18 21:43:15 +0800

    cors change

--- server.ts
+++ server.ts
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "user-management-system-frontend2-jpz1.vercel.app",
+    origin: "https://user-management-system-frontend-zeta.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
