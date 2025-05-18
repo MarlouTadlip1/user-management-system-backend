0e5adecced1932dd738195d6e139852ea8a9cc08
MarlouTadlip <tadlipmarlou@gmail.com>
2025-05-18 18:37:25 +0800

    changed cors origin

--- server.ts
+++ server.ts
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:4200",
+    origin: "https://user-management-system-frontend2.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
